f42e16d5126c71b928796bde0434ce8ed0dcd229
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2014-05-12 17:09:26 -0400

    minor spacing edits

--- make-pdf.sh
+++ make-pdf.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-pandoc -N --toc --toc-depth=2 --highlight-style=zenburn \
-	       --template=modern.tex --variable mainfont=Georgia \
-	              --variable sansfont=Arial --variable fontsize=12pt \
-		             -V monofont:Monaco --latex-engine=xelatex \
-			            -V urlcolor:amber \
-				           IntroToGitForScientists.md \
-					          -o IntroToGitForScientists.pdf 
+pandoc -N --toc --toc-depth=2 --highlight-style=zenburn --template=modern.tex --variable mainfont=Georgia --variable sansfont=Arial --variable fontsize=12pt -V monofont:Monaco --latex-engine=xelatex -V urlcolor:amber IntroToGitForScientists.md -o IntroToGitForScientists.pdf 
